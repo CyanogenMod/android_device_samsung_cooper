cc8df5436ca8b9ac82180867d2869368c03563b3
Kolja Dummann <k.dummann@gmail.com>
2011-06-27 00:51:29 +0200

    cooper 2.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo galaxyace-eng
-add_lunch_combo galaxyace-userdebug
+add_lunch_combo cooper-eng
+add_lunch_combo cooper-userdebug
